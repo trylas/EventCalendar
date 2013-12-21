82ef571ba43b3114edfcc88f1e384c39704da8f0
dalli <dalli@dalli>
2013-12-21 11:07:07 +0000

    almost done

--- public_html/eventCalendar.js
+++ public_html/eventCalendar.js
@@ -1,11 +1,32 @@
 function dateObj() {
-    this.d = new Date();
-    this.day = this.d.getDate();
-    this.numDay = this.d.getDay();
-    this.month = parseInt(this.d.getMonth());
-    this.year = this.d.getFullYear();
+    this.now = new Date();
+    this.day = this.now.getDate();
+    this.numDay = this.now.getDay();
+    this.month = parseInt(this.now.getMonth());
+    this.year = this.now.getFullYear();
     var self = this;
 
+//
+//function changeDate takes negative or positive number
+//to take next or previous month
+//
+    this.changeDate = function(Arg) {
+        var Arg;
+        var number = Arg > 0 ? -1 : 1;
+        var current = new Date(this.year, this.month + number, 1);
+        this.now = current;
+        this.numDay = current.getDay();
+        this.month = parseInt(current.getMonth())
+        this.year = current.getFullYear();
+    }
+
+//    
+//GetMonth replace number of Mont to Name
+//
+    this.getNameMonth = function() {
+        var month = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+        return month[this.month];
+    }
     this.getDaysMonth = function(year, month) {
         if (typeof (month) === 'undefined')
             month = this.month;
@@ -16,14 +37,29 @@ function dateObj() {
         return this.daysInMonth;
     }
     this.getPlainDays = function() {
-        var date = new Date(), y = date.getFullYear(), m = date.getMonth();
+        var y = this.year;
+        var m = this.month;
         var firstDay = new Date(y, m, 1);
-        weekday = new Array(6, 5, 4, 3, 2, 1);
+        var weekday = new Array(6, 0, 1, 2, 3, 4, 5);
         return weekday[firstDay.getDay()];
     }
+
+//
+//mark current day and mark events in current month
+//
+//
     this.markEvents = function(month, year) {
-        month = this.month + 1;
-        year = this.year;
+        var month = this.month + 1;
+        var year = this.year;
+        var day = this.day;
+        var today = this.checkMonth();
+
+        if (today) {
+            $('#' + day).addClass('today');
+        }
+
+//Mark events in current month
+
         var matches = Calendar.events.filter(function(val, index, array) {
             return parseInt(val.Month) === month && parseInt(val.Year) === year;
         });
@@ -31,46 +67,116 @@ function dateObj() {
             $('#' + matches[i].Day).addClass('event');
         }
     }
+//
+//Check if month displayed is current month
+//set mil sec to 0 to compare dates
+//
+    this.checkMonth = function() {
+        var x = new Date();
+        var y = this.now;
+        return x.getMonth() === y.getMonth();
+    }
+
+//
+//Building PlainDays + Days without events
+//
 
     this.buildMonth = function() {
         var numDays = this.getDaysMonth();
         var plainDays = this.getPlainDays();
         var html = '';
 
-//
-//Building PlainDays + Days without events
-//
+        $('#calendarNav li').eq(1).html($.trim(this.getNameMonth()));
+
         $('#month').append(function() {
             for (var i = -plainDays; i < numDays; i++) {
                 if (i <= -1) {
                     html += "<li class='eventCal empty'></li>";
-                } else {
+                }
+                else {
                     html += "<li id='" + parseInt(i + 1) + "'  class='eventCal'><a href='#'>" + parseInt(i + 1) + "</a></li>";
                 }
             }
             return html;
         });
+        this.markEvents();
+        this.getMonthEvents();
+        this.calenHeight();
+        this.getDayEvents();
     }
+    
+    this.calenHeight = function(){
+        var t = $('#calendarSlider').css("height");
+        console.log(t);
+    }
+
+//
+//
+//
+    this.sortEvents = function(array, field) {
+        var field;
+
+        function mycomparator(a, b) {
+            return parseInt(a[field]) - parseInt(b[field]);
+        }
+
+        var arr = array.sort(mycomparator);
+        return arr;
+    }
+
+//
+//getMonths events and siplay it on first view
+//
+
+    this.getMonthEvents = function() {
+        var month = this.month + 1;
+        var year = this.year;
+        var matches = Calendar.events.filter(function(val, index, array) {
+            return parseInt(val.Month) === parseInt(month) && parseInt(val.Year) === parseInt(year);
+        });
+
+        var matchLength = matches.length;
+        var html = '<ul>';
+
+        matches = this.sortEvents(matches, 'Day');
+        if (matches[0]){
+            html += '<li>' + self.getNameMonth() + ' - next events:</li>';
+        }else{
+            html += '<li>In ' + self.getNameMonth() + ' - there are no events.</li>';
+        }
+        for (i = 0; i < matchLength; i++) {
+            var date = matches[i].Day + '-' + matches[i].Month + '-' + matches[i].Year;
+            var hour = matches[i].Hour;
+            html += '<li>';
+            html += '<time date=' + date + '><em>' + date + '</em><small>' + hour + '</small></time>';
+            html += '<a href="#" class="eventsToggle">' + matches[i].Title + '</a></li>';
+        }
+        html += '</ul>';
+        $('#Day-events-content').html(html);
+    }
+
 //        
-//GetEvents - we selecting day by click and get Events
+//getDayEvents - we selecting day by click and get Events
 //
 //get id from list
 //and find events by date
 
-    this.getEvents = function() {
+    this.getDayEvents = function() {
         var year = this.year;
 
         $('#month').on("click", ".event", function() {
-            
             var id = $(this).attr('id');
             var matches = Calendar.events.filter(function(val, index, array) {
                 return parseInt(val.Day) === parseInt(id) && parseInt(val.Year) === parseInt(year);
             });
+            matches = self.sortEvents(matches, 'Hour');
             var matchLength = matches.length;
 
             $('#Day-events-content').html(function() {
                 $('#month li').removeClass("active");
                 var html = '<ul>';
+                html += '<li>' + self.getNameMonth() + ' ' + id + ' events:</li>';
+
                 for (i = 0; i < matchLength; i++) {
                     var date = matches[i].Day + '-' + matches[i].Month + '-' + matches[i].Year;
                     var hour = matches[i].Hour;
@@ -84,21 +190,30 @@ function dateObj() {
                 return html;
             });
         });
+    }
+//END getDayEvents
 
-//END Get Events
-    }
+
+//
+//Slide Up Events Details
+//
+//
     this.slideUpComm = function() {
-        $("#Day-events-content p").addClass("hidden").slideUp("fast");
+        $("#Day-events-content ul").remove().slideUp("fast");
+        $("#Day-events-content h3").remove();
     }
+
+
+
     this.showhideEvents = function() {
 
         $("#Day-events-content").on("click", ".eventsToggle", function() {
             var elem = $(this).next();
             if (elem.hasClass("hidden")) {
-                self.slideUpComm();
+                $("#Day-events-content p").addClass("hidden").slideUp("fast");
                 $(elem).removeClass("hidden").slideDown("slow");
             } else {
-                self.slideUpComm();
+                $("#Day-events-content p").addClass("hidden").slideUp("fast");
             }
         });
     }
@@ -117,17 +232,21 @@ function dateObj() {
                         "opacity": 1
                     }, 500, function() {
                 $("#month li").remove();
-                $("#Day-events-content").remove();
             });
-
+            $("#month").animate(
+                    {
+                        "left": 0,
+                        "opacity": 100
+                    }, 500, function() {
+                self.changeDate(slideWidth);
+                self.buildMonth();
+            });
         });
     }
 }
 $(document).ready(function() {
     var calendar = new dateObj();
     calendar.buildMonth();
-    calendar.markEvents();
-    calendar.getEvents();
     calendar.showhideEvents();
     calendar.slideCalendar();
 });
--- public_html/test.js
+++ public_html/test.js
@@ -21,9 +21,25 @@ var Calendar =
                     "Day": "24",
                     "Month": "12",
                     "Year": "2013",
+                    "Hour": "12:00",
+                    "Title": "Christmas - gifts",
+                    "Desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non neque ac sem ultrices sodales sed eget lectus. Phasellus quis varius massa. Sed aliquet ante vitae ultrices mattis. In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas tincidunt lorem lectus, a facilisis felis elementum id. Nullam dignissim magna ut consequat consequat."
+                },
+                {
+                    "Day": "1",
+                    "Month": "1",
+                    "Year": "2014",
                     "Hour": "20:00",
                     "Title": "Christmas - gifts",
                     "Desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non neque ac sem ultrices sodales sed eget lectus. Phasellus quis varius massa. Sed aliquet ante vitae ultrices mattis. In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas tincidunt lorem lectus, a facilisis felis elementum id. Nullam dignissim magna ut consequat consequat."
-                }
+                },
+                {
+                    "Day": "12",
+                    "Month": "12",
+                    "Year": "2013",
+                    "Hour": "18:00",
+                    "Title": "EU driving",
+                    "Desc": "I've got it.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non neque ac sem ultrices sodales sed eget lectus. Phasellus quis varius massa. Sed aliquet ante vitae ultrices mattis. In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas tincidunt lorem lectus, a facilisis felis elementum id. Nullam dignissim magna ut consequat consequat."
+                },
             ]
         } 
